8944c7570757e7fcf8cc8f6200582262e6fb143d
Micosil <mico@micodev.es>
2021-08-05 16:43:05 +0200

    Added Simplex noise

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import * as THREE from "three";
 import { Component } from "react";
 import { Vector3 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { makeNoise2D } from "open-simplex-noise";
+import { makeRectangle, valueNoise2D } from "fractal-noise";
 
 class App extends Component {
   componentDidMount() {
@@ -19,22 +21,31 @@ class App extends Component {
     this.mount.appendChild(renderer.domElement);
     //Setup geometry
     const scale = 20;
-    const columns = window.innerWidth / scale;
-    const rows = window.innerHeight / scale;
+    const noiseScale = 300;
+    const columns = Math.round(window.innerWidth / scale);
+    const rows = Math.round(window.innerHeight / scale);
 
     const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
 
+    //Create point cloud
     let triangles = [];
+    const noise2D = makeRectangle(columns, rows, makeNoise2D(Date()), {
+      frequency: 0.04,
+      octaves: 8,
+    });
+    console.log(noise2D);
+
     for (let y = -rows / 2; y < rows / 2; y++) {
       let columnVectors = [];
       for (let x = -columns / 2; x < columns / 2; x++) {
+        let noiseValue = noise2D[x + columns / 2][y + rows / 2];
         columnVectors.push(
-          new Vector3(x * scale, y * scale, Math.random() * 100)
+          new Vector3(x * scale, y * scale, noiseValue * noiseScale)
         );
       }
       triangles.push(columnVectors);
     }
-
+    //Draw triangles for points
     for (let y = 0; y < triangles.length - 1; y++) {
       for (let x = 0; x < triangles[y].length - 1; x++) {
         let points = [];
@@ -49,12 +60,15 @@ class App extends Component {
       }
     }
 
+    //Camera initial setup
     camera.position.set(window.innerWidth / 2, window.innerHeight / 2, 700);
     camera.lookAt(0, 0, 0);
 
-    //Animation loop
+    //Controls setup
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.update();
+    console.log("hola");
+    //Animation loop
     var animate = function () {
       requestAnimationFrame(animate);
       controls.update();
